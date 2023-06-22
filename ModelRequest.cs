0c81c824fc5bb75e96c8cb63d1857a5c3b05adcc
Evandru <evandro_remolli.t@outlook.com>
2023-06-22 11:48:40 -0300

    add: ModelRequest model class

---
+++ ModelRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoCurrency_Console
+{
+    public class ModelRequest
+    {
+        public string asset_id_base { get; set; }
+        public RateUnit[] rates { get; set; }
+    }
+    public class RateUnit
+    {
+        public string time { get; set; }
+        public string asset_id_quote { get; set; }
+        public double rate { get; set; }
+    }
+}
