4730486f134b080c940035dd72effdaadd8f866f
Evandru <evandro_remolli.t@outlook.com>
2023-06-22 12:39:02 -0300

    add: HUD and verified null response

--- Program.cs
+++ Program.cs
@@ -1,16 +1,31 @@
-﻿using CryptoCurrency_Console;
+﻿using CryptoCurrency_Console.Models;
 using RestSharp;
 
+// HUD
+Console.WriteLine("Welcome to CryptoCurrency");
+Console.WriteLine();
+Console.WriteLine("asset id base => BTC ");
+Console.WriteLine();
+Console.Write("Insert an 'asset id base': ");
 // Base crypto for request
-string asset_id_base = "BTC";
+string asset_id_base = Console.ReadLine();
 
 // Request
 var options = new RestClientOptions("https://rest.coinapi.io");
 var client = new RestClient(options);
 var request = new RestRequest($"/v1/exchangerate/{asset_id_base}");
 request.AddHeader("X-CoinAPI-Key", "11CA632B-F6B4-419E-8E88-65246C594C97");
-//                                        This <T> is the Model class deserialized
+// This GetAsync<T> is the Model class that will be automatic deserialized
 var requestDeserialized = await client.GetAsync<ModelRequest>(request);
+
+// Verifying null response
+if (requestDeserialized.rates == null)
+{
+    Console.WriteLine();
+    Console.WriteLine("Null rates");
+    Console.WriteLine("Exiting...");
+    Environment.Exit(0);
+}
 
 // View
 Console.WriteLine(requestDeserialized.asset_id_base);
