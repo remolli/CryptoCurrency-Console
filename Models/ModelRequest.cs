d1a1fcdc7ece283f499e2498ea4b0532140a0367
Evandru <evandro_remolli.t@outlook.com>
2023-06-22 12:36:46 -0300

    fix: organization

--- Models/ModelRequest.cs
+++ Models/ModelRequest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CryptoCurrency_Console
+namespace CryptoCurrency_Console.Models
 {
     public class ModelRequest
     {
