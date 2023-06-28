57045255af3d7154079da11fc241708e083d8937
Evandru <evandro_remolli.t@outlook.com>
2023-06-28 19:28:24 -0300

    modify: new models by new API

---
+++ Models/GetSymbolModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CryptoCurrency_Console.Models
+{
+    public class GetSymbolsModel
+    {
+        public Dictionary<string,GetSymbolModel> models { get; set; }
+    }
+
+    public class GetSymbolModel
+    {
+        public string base_currency { get; set; }
+        public double base_currency_scale { get; set; }
+        public string counter_currency { get; set; }
+        public double counter_currency_scale { get; set; }
+        public double min_price_increment { get; set; }
+        public double min_price_increment_scale { get; set; }
+        public double min_order_size { get; set; }
+        public double min_order_size_scale { get; set; }
+        public double max_order_size { get; set; }
+        public double max_order_size_scale { get; set; }
+        public double lot_size { get; set; }
+        public double lot_size_scale { get; set; }
+        public string status { get; set; }
+        public double id { get; set; }
+        public double auction_price { get; set; }
+        public double auction_size { get; set; }
+        public string auction_time { get; set; }
+        public double imbalance { get; set; }
+    }
+}
+
---
+++ Models/GetTickerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CryptoCurrency_Console.Models
+{
+
+    public class GetTickerModel
+    {
+        public string symbol { get; set; }
+        public double price_24h { get; set; }
+        public double volume_24h { get; set; }
+        public double last_trade_price { get; set; }
+    }
+}
